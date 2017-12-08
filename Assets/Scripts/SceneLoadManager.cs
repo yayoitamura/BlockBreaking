7389897d12f30bd218409493da41fea4aa3ff430
yayoitamura <gh3520900@gmail.com>
2017-12-08 19:43:27 +0900

    Add button to switch scenes

---
+++ Assets/Scripts/SceneLoadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void LoadScene () {
+		Debug.Log ("Load Scene");
+		SceneManager.LoadScene (1);
+	}
+}
