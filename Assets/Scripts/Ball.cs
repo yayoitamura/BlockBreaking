3af182d56a4f987ebf16edcd47e8eb31da8fe6ee
yayoitamura <gh3520900@gmail.com>
2017-12-10 00:34:40 +0900

    Move the position of the Ball follow the Paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private const float PADDLE_TO_BALL_VECTOR = 0.3f;
+	private GameObject paddle;
+	private Vector2 paddlePositon;
+	void Start () {
+		paddle = GameObject.Find ("Paddle");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		paddlePositon = paddle.transform.position;
+		paddlePositon.y += PADDLE_TO_BALL_VECTOR;
+		transform.position = paddlePositon;
+
+	}
+}
