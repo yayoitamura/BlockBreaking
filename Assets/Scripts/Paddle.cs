63be2e94337cbefacfa3cc037a2daeba014e2d1a
yayoitamura <gh3520900@gmail.com>
2017-12-09 20:48:41 +0900

    Make the position of the Paddle follow the mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 mousePosi = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePosi.y = transform.position.y;
+		transform.position = mousePosi;
+	}
+}
