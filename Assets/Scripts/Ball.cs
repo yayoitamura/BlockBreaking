4a804c7c2de4a5edb9a9faf319eefb6d89118d26
yayoitamura <gh3520900@gmail.com>
2017-12-10 15:45:22 +0900

    Move ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-
+	Rigidbody2D rb2d;
+	private float thrust = 1000f;
 	private const float PADDLE_TO_BALL_VECTOR = 0.3f;
 	private GameObject paddle;
 	private Vector2 paddlePositon;
-	void Start () {
+
+	private enum GAME_STATE {
+		READY,
+		PLAY,
+		};
+		private GAME_STATE game = GAME_STATE.READY;
+
+		void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
 		paddle = GameObject.Find ("Paddle");
 
 	}
 
-	// Update is called once per frame
 	void Update () {
-		paddlePositon = paddle.transform.position;
-		paddlePositon.y += PADDLE_TO_BALL_VECTOR;
-		transform.position = paddlePositon;
+		switch (game) {
+			case GAME_STATE.READY:
+
+				paddlePositon = paddle.transform.position;
+				paddlePositon.y += PADDLE_TO_BALL_VECTOR;
+				transform.position = paddlePositon;
+
+				if (Input.GetMouseButton (0)) {
+					rb2d.AddForce (Vector2.up * thrust);
+					game = GAME_STATE.PLAY;
+				}
+				break;
+
+			case GAME_STATE.PLAY:
+
+				break;
+		}
 
 	}
 }
