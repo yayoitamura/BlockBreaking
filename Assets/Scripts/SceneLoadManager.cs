5eae61f5228ba4baae01cbafb495b8f88d6b740a
yayoitamura <gh3520900@gmail.com>
2017-12-19 23:18:16 +0900

    Remove some dead code

--- Assets/Scripts/SceneLoadManager.cs
+++ Assets/Scripts/SceneLoadManager.cs
@@ -1,37 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () { }
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
 	public void LoadScene (int scene) {
-		Debug.Log (scene);
 		SceneManager.LoadScene (scene);
 	}
 
-	// private IEnumerator LoadSceneCol (int scene) {
-
-	// }
-
-	private IEnumerator SceneLoad (int scene) {
-
+	private IEnumerator BeforeLoadScene (int scene) {
 		yield return new WaitForSeconds (2f);
-		SceneManager.LoadScene (scene);
+		LoadScene (scene);
 	}
 
 	public void BlockDestroyed () {
 		if (Block.breakableCount <= 0) {
-			// LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
-			StartCoroutine ("SceneLoad", SceneManager.GetActiveScene ().buildIndex + 1);
+			StartCoroutine ("BeforeLoadScene", SceneManager.GetActiveScene ().buildIndex + 1);
 		}
 	}
 }
